0df54e67953cb1e212addf6673ca0e766a3d3b7e
Yann-La-Baguette <ydipadova@gmail.com>
2023-02-02 08:11:45 +0100

    Add classes for detcting cursor

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +15,25 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+/*void MainWindow::mousePressEvent(QMouseEvent *e)
+{
+    if(e->button() == Qt::RightButton)
+    {
+        qDebug() << "Right mouse click!" << Qt::endl;
+    }
+}*/
+
+QPoint MainWindow::PositionSouris(){
+    QPoint p = ui->widget->mapFromGlobal(QCursor::pos());
+    ui->label->setText(QString::number(p.rx()));
+    ui->label->setText(QString::number(p.ry()));
+
+    return p;
+}
+
+void QWidget::mousePressEvent(QMouseEvent *event)
+{
+    if (event->button() == Qt::LeftButton) {
+
+    }
+}
